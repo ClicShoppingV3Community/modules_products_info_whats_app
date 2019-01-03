c4c92b4c2326ccc1f94e87811e32f66356eea3b8
ClicShopping - e-commerce Solution B2B B2C <39338579+ClicShopping@users.noreply.github.com>
2019-01-03 12:05:25 +0100

    update header info

--- sources/template/Default/modules/modules_products_info/content/products_info_whatsapp.php
+++ sources/template/Default/modules/modules_products_info/content/products_info_whatsapp.php
@@ -1,5 +1,15 @@
 <?php
-  use ClicShopping\OM\CLICSHOPPING;
+/**
+ *
+ *  @copyright 2008 - https://www.clicshopping.org
+ *  @Brand : ClicShopping(Tm) at Inpi all right Reserved
+ *  @Licence GPL 2 & MIT
+ *  @licence MIT - Portion of osCommerce 2.4
+ *  @Info : https://www.clicshopping.org/forum/trademark/
+ *
+ */
+
+use ClicShopping\OM\CLICSHOPPING;
 ?>
 <div class="<?php echo $text_position; ?> col-md-<?php echo $content_width; ?> moduleProductsInfoWhatsappGeneral">
   <a href="https://wa.me/<?php echo $phone_number; ?>?text=I'm%20interested%20in <?php echo $product_name . ' - ' . $products_model . ' - ' . $products_url; ?>" class="btn btn-success" role="button" target="_blank">
